3a568b1f147d11b4640f2321c8613037d97d6539
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2023-09-26 16:32:20 +0600

    fixing ongoing

--- client/src/components/Layout/RootLayout.jsx
+++ client/src/components/Layout/RootLayout.jsx
@@ -1,26 +1,46 @@
 import { useEffect, useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import {
+    json,
+    Outlet,
+    useActionData,
+    useLocation,
+    useNavigate,
+} from "react-router-dom";
 import axios from "axios";
 import Header from "../Layout/Header";
 import Footer from "./Footer";
 const RootLayout = (props) => {
-    console.log("Root layout is being reloaded");
+    console.log("root layout");
+    const location = useLocation();
     const [userName, setUserName] = useState();
     const [userId, setUserId] = useState();
     useEffect(() => {
         const showMe = async () => {
-            let res = await axios.get(
-                "http://localhost:5000/api/v1/users/showMe",
-                {
-                    withCredentials: true,
+            let res;
+            try {
+                res = await axios.get(
+                    "http://localhost:5000/api/v1/users/showMe",
+                    {
+                        withCredentials: true,
+                    }
+                );
+            } catch (err) {
+                if (err.message) {
+                    throw json({ message: err.message }, { status: 500 });
+                } else if (err.response.status === 401) {
+                    setUserName(null);
+                    setUserId(null);
                 }
-            );
+                return;
+            }
             console.log("Entered in root useEffect");
-            setUserName(res.data.user.name);
-            setUserId(res.data.user.userId);
+            if (res.data.user) {
+                setUserName(res.data.user.name);
+                setUserId(res.data.user.userId);
+            }
         };
         showMe();
-    }, [userName, userId]);
+    }, [location]);
     return (
         <>
             <Header user={{ userName, userId }}></Header>
